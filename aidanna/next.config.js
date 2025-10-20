2c948fa5f2b45652bec52684e04124503a8a18e4
Taofeeq Mukadaz <153239219+Mukadaaazzzz@users.noreply.github.com>
2025-10-20 15:59:58 +0000

    gfbug

--- aidanna/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    unoptimized: true, // Disable image optimization for Netlify
-  },
-};
-
-module.exports = nextConfig;
