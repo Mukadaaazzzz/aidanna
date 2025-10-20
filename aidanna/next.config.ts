282e514198f08b13d1433397044fe26828f3e8cb
Taofeeq Mukadaz <153239219+Mukadaaazzzz@users.noreply.github.com>
2025-10-20 14:48:40 +0000

    third commit

--- aidanna/next.config.ts
+++ aidanna/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+ images: {
+    unoptimized: true, // Disables image optimization
+  },
 };
 
 export default nextConfig;
